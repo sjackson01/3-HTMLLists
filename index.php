ed735e4428d5450b1eb87e85f2b542e0446869a4
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-08 16:03:40 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Experience VR</title>
+    </head>
+  
+    <body>
+      <h1>Experience VR</h1> 
+      
+      <p>A simple blog about virtual reality experiences</p>
+      
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Article</li>  
+        <li>
+          Contact
+          <ul>
+          <li>Email</li>
+          <li>Phone</li>
+          <li>Address</li>  
+          </ul>
+        </li>
+      </ul>
+      
+      <p>Virtual Reality is becoming well known as a form of entertainment, but it's also   finding its way into fields like education, industrial design, healthcare and so much more!</p>
+      
+      <h2>Latest VR Articles</h2>
+      
+      <h3>How Schools Use Virtual Reality to Improve Education</h3>
+      
+      <p>This is blog entry one about vitual reality. We could have added the ready made code but thats not fun so this is what we end up with one long run on sentence.</p>
+      
+      <h3>The Advantages of VR Simulation</h3>
+      
+      <p>This is blog entry one about vitual reality. We could have added the ready made code but thats not fun so this is what we end up with one long run on sentence</p>
+     
+      <h3>Top VR Resources</h3>
+      <ol>
+      <li>Learn to create educational experiences in VR</li>
+      <li>Virtural Reality in Entertainment</li>
+      <li>Interact with building and products in VR</li>
+      <li>Use VR for teleconferencing and social media</li>
+      </ol>
+       
+    </body>
+  
+</html>
